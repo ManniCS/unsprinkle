4586ff6c779851f7c0a5861cf1bc22e7ab764e8a
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-22 18:38:02 -0400

    Do not allow last tag to have a right margin; remove unused width: max-content property, and use padding instead of line-height to be more precise about background space given to each li element

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -55,6 +55,7 @@ const Tags = styled.ul`
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+  padding: 4px 0;
 `;
 
 const Tag = styled.li`
@@ -63,10 +64,12 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
-  width: max-content;
+
   display: inline;
-  line-height: 1.8rem;
-  margin-right: 8px;
+
+  &:not(:last-of-type) { 
+    margin-right: 8px;
+  }
 `;
 
 export default PhotoGridItem;
